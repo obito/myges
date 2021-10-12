bffbc15cabc471ef395963022ab01634d203fd9a
youssef <me@bnkd.me>
2021-10-12 23:19:48 +0200

    add discord integration

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ Explication de chaque librairie (tous hebergées en local):
 * PureCounter, pour les nombres qui défilent dans la catégorie "Nos valeurs" de la home page.
 
 Besoin d'installer composer et la lib php-mailer pour utiliser le form.
+
+## Email
+
+Le form de contact envoie un e-mail à vous même contenant toutes les informations de l'utilisateur qui vient d'utiliser le formulaire.
+
+Vous pouvez changer le serveur SMTP dans le fichier forms/contact.php, mais par défaut il utilise le serveur SMTP de SendGrid.
+## Discord
+
+Le form de contact envoie également un message en DM sur Discord, il faut d'abord:
+
+* Inviter le bot Discord dans un serveur, pour qu'il puisse vous DM
+* Changer le recipient_dm dans le forms/contact.php
+* Envoyer un message au bot pour qu'il puisse vous avoir dans sa liste de DM
--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -7,35 +6,85 @@ use PHPMailer\PHPMailer\Exception;
 
 require '../vendor/autoload.php';
 
-$errMsg = '';
-$email = '';
 
-//Apply some basic validation and filtering to the subject
+function MakeRequest($endpoint, $data) {
+    // Set endpoint
+    $url = "https://discord.com/api/".$endpoint."";
+
+    // Encodez les données, car Discord vous demande d'envoyer des données json.
+    $data = json_encode($data);
+
+    // Initialize new curl request
+    $ch = curl_init();
+    // $f = fopen('request.txt', 'w'); // écrire la request dans un fichier .txt (debugging)
+
+    // Set headers, data etc..
+    curl_setopt_array($ch, array(
+        CURLOPT_URL            => $url, 
+        CURLOPT_HTTPHEADER     => array(
+            'Authorization: Bot TOKEN_ICI', // Mettre le token bot discord à la place de "TOKEN_ICI"
+            "Content-Type: application/json",
+            "Accept: application/json"
+        ),
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_FOLLOWLOCATION => 1,
+        CURLOPT_VERBOSE        => 1,
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_POSTFIELDS => $data,
+        //CURLOPT_STDERR => $f,
+    ));
+
+    $request = curl_exec($ch);
+
+    echo $request;
+    curl_close($ch);
+    return json_decode($request, true);
+}
+
+// Ouvrir la liste de DMs d'abord
+$newDM = MakeRequest('/users/@me/channels', array("recipient_id" => "892394978213498930")); // change recipient_id with discord id
+
+$errMsg = '';
+
+// Appliquer une validation et un filtrage de base au sujet.
 if (array_key_exists('subject', $_POST)) {
     $subject = substr(strip_tags($_POST['subject']), 0, 255);
 } else {
-    $subject = 'No subject given.';
+    $subject = "Aucun";
 }
 
-//Apply some basic validation and filtering to the query
+//Appliquer une validation et un filtrage de base au message.
 if (array_key_exists('message', $_POST)) {
-    //Limit length and strip HTML tags
+    // Limiter la longueur et supprimer les balises HTML
     $message = substr(strip_tags($_POST['message']), 0, 16384);
 } else {
-    $errMsg = 'No message given.';
+    $errMsg = "Aucun message n'a été donné.";
 }
 
-//Apply some basic validation and filtering to the name
+// Appliquer une validation et un filtrage de base au nom.
 if (array_key_exists('name', $_POST)) {
-    //Limit length and strip HTML tags
+    // Limiter la longueur et supprimer les balises HTML
     $name = substr(strip_tags($_POST['name']), 0, 255);
 } else {
-    $errMsg = 'No name given.';
+    $errMsg = "Aucun nom n'a été donné";
+}
+
+// Appliquer une validation et un filtrage de base à l'email.
+if (array_key_exists('email', $_POST)) {
+    // Limiter la longueur et supprimer les balises HTML
+    $email = substr(strip_tags($_POST['email']), 0, 255);
+} else {
+    $errMsg = "Aucun e-mail a été donné.";
 }
 
 $toEmail = "me@bnkd.me";
 
 if ($errMsg == '') {
+    // Vérifie si le DM est bien présent, si oui, envoyer le message.
+    if(isset($newDM["id"])) {
+        $newMessage = MakeRequest("/channels/".$newDM["id"]."/messages", array("content" => "Message de " . $name . " (". $email . ") " . "Sujet: " . $subject . "\Body: " . $message));
+    }
+
     $mail = new PHPMailer();
     $mail->IsSMTP();
     $mail->Host = 'smtp.sendgrid.net'; //Adresse IP ou DNS du serveur SMTP
@@ -50,14 +99,14 @@ if ($errMsg == '') {
 
     $mail->setFrom($toEmail, (empty($name) ? 'Contact form' : $name));
 
-    $mail->From       = trim("me@bnkd.me"); //L'email à afficher pour l'envoi
-    $mail->FromName   = trim($_POST["name"]); //L'alias de l'email de l'emetteur
+    $mail->From       = "me@bnkd.me"; //L'email à afficher pour l'envoi
+    $mail->FromName   = $name; //L'alias de l'email de l'emetteur
 
     $mail->AddAddress("me@bnkd.me");
 
-    $mail->Subject = 'Contact form: ' . $subject;
+    $mail->Subject = 'Formulaire de contact: ' . $subject;
     $mail->WordWrap   = 50; //Nombre de caracteres pour le retour a la ligne automatique
-    $mail->Body = "Message contact form: \n\n" . $message;
+    $mail->Body = "Venant de: " . $email . " Message: \n\n" . $message;
     $mail->IsHTML(false); //Préciser qu'il faut utiliser le texte brut
 
 
